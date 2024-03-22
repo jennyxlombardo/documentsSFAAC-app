5df1078bac77f532bf0474147fcad5b3cb5b8326
Jennifer Lombardo <JENNIFERLOMBARDO9@HOTMAIL.COM>
2024-03-22 15:42:49 -0300

    responsive navbar

--- app/components/navBar.jsx
+++ app/components/navBar.jsx
@@ -3,15 +3,15 @@ import Image from "next/image"
 
 export default function NavBar() {
     return (
-      <div className="flex flex-row justify-around items-center h-36 text-xl sticky w-screen border-nBlue border-b">
-        <button>Home</button>
-        <button>Services</button>
+      <div className="flex flex-col p-10 md:p-0 md:flex-row md:justify-around justify-between items-center h-36 text-xl sticky w-screen md:border-nBlue md:border-b">
+        <button className="p-2">Home</button>
+        <button className="p-2">Services</button>
         <Image  src="/logo1.png" alt="logo" width="200" height="200"
           className="">
          
         </Image>
-        <button>About</button>
-        <button>Contact</button>
+        <button className="p-2">About</button>
+        <button className="p-2">Contact</button>
       </div>
     );
 }
