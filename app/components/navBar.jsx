900abe17a586a7b10e3b90e5525d4997bbf7048c
Jennifer Lombardo <JENNIFERLOMBARDO9@HOTMAIL.COM>
2024-03-22 15:45:19 -0300

    Updating nav again

--- app/components/navBar.jsx
+++ app/components/navBar.jsx
@@ -4,14 +4,16 @@ import Image from "next/image"
 export default function NavBar() {
     return (
       <div className="flex flex-col p-10 md:p-0 md:flex-row md:justify-around justify-between items-center h-36 text-xl sticky w-screen md:border-nBlue md:border-b">
-        <button className="p-2">Home</button>
-        <button className="p-2">Services</button>
+        <button className="p-2 hidden md:block">Home</button>
+        <button className="p-2 hidden md:block">Services</button>
         <Image  src="/logo1.png" alt="logo" width="200" height="200"
           className="">
          
         </Image>
         <button className="p-2">About</button>
-        <button className="p-2">Contact</button>
+            <button className="p-2">Contact</button>
+            <button className="p-2 md:hidden block">Home</button>
+        <button className="p-2 md:hidden block">Services</button>
       </div>
     );
 }
